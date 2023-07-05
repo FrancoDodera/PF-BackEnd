93b0d3bc1c8557152ff372a6de3951377324bbeb
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-05 18:22:11 -0400

    cambio de configuraciones para peticiones

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,13 @@ const usersRouter = require('./routes/UsersRoute');
 
 app.use(express.json());
 // // ... Otros middlewares
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // // Rutas
 // const carRoutes = require('./routes/carRoutes');
