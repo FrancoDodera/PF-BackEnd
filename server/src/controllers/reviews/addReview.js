51cbb130449d45e24f812003dc25aa1c3b212691
Barbara Ruiz <baarbi.sona@gmail.com>
2023-07-18 18:58:58 -0300

    review

--- server/src/controllers/reviews/addReview.js
+++ server/src/controllers/reviews/addReview.js
@@ -1,20 +1,71 @@
-const Reviews = require('../../models/Reviews')
+const Reviews = require("../../models/Reviews");
 
-const addReview=async(id_user,id_car,coment,value)=>{
-    try {
-        const newReview = await new Reviews({
-           id_car:id_car,
-           id_user:id_user,
-           coment:coment,
-           value:value
-        })
-       
-        await newReview.save()
-        
-    } catch (error) {
-        throw error
-    }
+//post
+const addReview = async (id_user, id_car, coment, value) => {
+  try {
+    const newReview = await new Reviews({
+      id_car: id_car,
+      id_user: id_user,
+      coment: coment,
+      value: value,
+    });
 
+    await newReview.save();
+  } catch (error) {
+    throw error;
+  }
+};
+
+//get review
+const getReview = async (id_user, id_car) => {
+  try {
+    const review = await Reviews.find({ id_car: id_car, id_user: id_user });
+    return review;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//get all reviews
+const getAllReviews = async () => {
+  try {
+    const reviews = await Reviews.find();
+    return reviews;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//delete review
+const deleteReview = async (id_user, id_car) => {
+  try {
+    const review = await Reviews.deleteOne({
+      id_car: id_car,
+      id_user: id_user,
+    });
+    return review;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateReview = async (id_user, id_car, coment, value) => {
+  try {
+    const review = await Reviews.updateOne(
+      { id_car: id_car, id_user: id_user },
+      { coment: coment, value: value }
+    );
+    return review;
+  } catch (error) {
+    throw error;
+  }
 }
 
-module.exports=addReview
+
+
+module.exports = 
+addReview, 
+getReview, 
+getAllReviews,
+updateReview, 
+deleteReview;
