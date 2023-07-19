2bdac85cb805f99a53f213f156d7351503254865
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-18 20:48:43 -0400

    reviews

--- server/src/controllers/reviews/addReview.js
+++ server/src/controllers/reviews/addReview.js
@@ -1,4 +1,5 @@
 const Reviews = require("../../models/Reviews");
+const User = require('../../models/UserModel')
 
 //post
 const addReview = async (id_user, id_car, coment, value) => {
@@ -20,8 +21,31 @@ const addReview = async (id_user, id_car, coment, value) => {
 //get review
 const getReview = async (id_car) => {
   try {
+    let response=[];
     const review = await Reviews.find({ id_car: id_car});
-    return review;
+    if(review.length>0){
+      await Promise.all(
+        review.map(async(elem)=>{
+          const dataUser=await User.findById(elem.id_user);
+          const body={
+            _id:elem._id,
+            id_user:{
+              id:dataUser._id,
+              name:dataUser.name,
+              lastNmae:dataUser.lastName,
+              user:dataUser.user,
+              image:dataUser.image
+            },
+            id_car:elem.id_car,
+            coment:elem.coment,
+            value:elem.value
+          }
+          response.push(body)
+        })
+      )
+    }
+    
+    return response;
   } catch (error) {
     throw error;
   }
