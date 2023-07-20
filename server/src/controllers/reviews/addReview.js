2bc5e559a973332abf6310a123a2a4ea3e30f486
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-19 21:44:37 -0400

    reviews arreglos

--- server/src/controllers/reviews/addReview.js
+++ server/src/controllers/reviews/addReview.js
@@ -1,30 +1,30 @@
 const Reviews = require("../../models/Reviews");
 const SaleDetail = require("../../models/VentaDetailModel");
 const Sale = require("../../models/SaleModel");
-const User = require('../../models/UserModel')
+const User = require("../../models/UserModel");
 
 const userHasCar = async (id_user, id_car) => {
   try {
     let count = 0;
-    const sale = await Sale.find({id_user});
+    const sale = await Sale.find({ id_user });
     if (sale.length > 0) {
       await Promise.all(
         sale.map(async (elem) => {
           const detailSale = await SaleDetail.find({
             id_car: id_car,
             id_venta: elem._id,
-          }) 
+          });
           if (detailSale.length > 0) {
             count += 1;
           }
         })
-      )
+      );
     }
-if (count > 0) {
-    return true;
-  } else {
-    return false;
-  }
+    if (count > 0) {
+      return true;
+    } else {
+      return false;
+    }
     // if (!saleDetail) {
     //   return false;
     // }
@@ -39,13 +39,12 @@ if (count > 0) {
   }
 };
 
-
 //post
 const addReview = async (id_user, id_car, coment, value) => {
-  const palabrotas = ['palabramala1', 'palabramala2', 'palabramala3'];
+  const palabrotas = ["palabramala1", "palabramala2", "palabramala3"];
   try {
-    const palabrasMalas = palabrotas.some(word => coment.includes(word));
-    if (palabrasMalas){
+    const palabrasMalas = palabrotas.some((word) => coment.includes(word));
+    if (palabrasMalas) {
       return "You can't post a review with these words";
     }
     const hasPurchase = await userHasCar(id_user, id_car);
@@ -57,7 +56,21 @@ const addReview = async (id_user, id_car, coment, value) => {
         value: value,
       });
       await newReview.save();
-      return newReview;
+      const userData = await User.findById(id_user);
+      const body = {
+        _id: newReview._id,
+        id_user: {
+          id: userData._id,
+          name: userData.name,
+          lastNmae: userData.lastName,
+          user: userData.user,
+          image: userData.image,
+        },
+        id_car: newReview.id_car,
+        coment: newReview.coment,
+        value: newReview.value,
+      };
+      return body;
     } else {
       return false;
     }
@@ -69,31 +82,30 @@ const addReview = async (id_user, id_car, coment, value) => {
 //get review
 const getReview = async (id_car) => {
   try {
-    
-    let response=[];
-    const review = await Reviews.find({ id_car: id_car});
-    if(review.length>0){
+    let response = [];
+    const review = await Reviews.find({ id_car: id_car });
+    if (review.length > 0) {
       await Promise.all(
-        review.map(async(elem)=>{
-          const dataUser=await User.findById(elem.id_user);
-          const body={
-            _id:elem._id,
-            id_user:{
-              id:dataUser._id,
-              name:dataUser.name,
-              lastNmae:dataUser.lastName,
-              user:dataUser.user,
-              image:dataUser.image
+        review.map(async (elem) => {
+          const dataUser = await User.findById(elem.id_user);
+          const body = {
+            _id: elem._id,
+            id_user: {
+              id: dataUser._id,
+              name: dataUser.name,
+              lastNmae: dataUser.lastName,
+              user: dataUser.user,
+              image: dataUser.image,
             },
-            id_car:elem.id_car,
-            coment:elem.coment,
-            value:elem.value
-          }
-          response.push(body)
+            id_car: elem.id_car,
+            coment: elem.coment,
+            value: elem.value,
+          };
+          response.push(body);
         })
-      )
+      );
     }
-    
+
     return response;
   } catch (error) {
     throw error;
@@ -124,12 +136,23 @@ const deleteReview = async (id) => {
 
 const updateReview = async (id, coment, value) => {
   try {
-    await Reviews.updateOne(
-      { _id: id },
-      { coment: coment, value: value }
-    );
+    await Reviews.updateOne({ _id: id }, { coment: coment, value: value });
     const review = await Reviews.findOne({ _id: id });
-    return review ;
+    const userData = await User.findById(review.id_user);
+      const body = {
+        _id: review._id,
+        id_user: {
+          id: userData._id,
+          name: userData.name,
+          lastNmae: userData.lastName,
+          user: userData.user,
+          image: userData.image,
+        },
+        id_car: review.id_car,
+        coment: review.coment,
+        value: review.value,
+      };
+    return body;
   } catch (error) {
     throw error;
   }
