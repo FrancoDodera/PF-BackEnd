fc8c95016b16d42a835e28cadeee7af37affcf39
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-25 14:00:00 -0400

    modificaciones auth

--- server/src/controllers/users/authentication.js
+++ server/src/controllers/users/authentication.js
@@ -1,45 +1,46 @@
-const User = require("../../models/UserModel")
+const User = require("../../models/UserModel");
 
+const authentication = async (body) => {
+  const { name, lastName, email, user, token, dni, type, image, password } =
+    body;
 
-const authentication=async (body)=>{
-    const {name,lastName,email,user,token,dni,type,image,password}=body
-    
-    try {
-        const emailFind=await User.findOne({email:email})
-        if(emailFind.tokenAuth == null){
-            await User.updateOne({_id:emailFind._id},{
-                tokenAuth:token
-            })
-            const user=await User.findById(emailFind._id)
-            return {acces:true,data:user}
-        }else{
-            const userFind=await User.findOne({email:user,tokenAuth:token});
-            if(userFind){
-                if(userFind.status){
-                    return {acces:true,data:userFind}
-                }else{
-                    return {acces:false,data:userFind}
-                }
+  try {
+    const emailFind = await User.findOne({ email: email });
+    if (emailFind) {
+      if (emailFind.tokenAuth == null) {
+        await User.updateOne(
+          { _id: emailFind._id },
+          {
+            tokenAuth: token,
+          }
+        );
+        const user = await User.findById(emailFind._id);
+        return { acces: true, data: user };
+      } else {
+            if(emailFind.email==email && emailFind.tokenAuth==token){
+                return { acces: true, data: emailFind };
             }else{
-                const newUser = new User({
-                    email:email,
-                    name:name,
-                    lastName:lastName,
-                    dni:dni,
-                    user:user,
-                    password:password,
-                    type:type,
-                    image:image,
-                    tokenAuth:token
-                })
-                await newUser.save()
-                return {acces:true,data:newUser}
+                return { acces: false};
             }
-        }
-        
-    } catch (error) {
-        throw error
+      }
+    } else {
+        const newUser = new User({
+          email: email,
+          name: name,
+          lastName: lastName,
+          dni: dni,
+          user: user,
+          password: password,
+          type: type,
+          image: image,
+          tokenAuth: token,
+        });
+        await newUser.save();
+        return { acces: true, data: newUser };
     }
-}
+  } catch (error) {
+    throw error;
+  }
+};
 
-module.exports=authentication
+module.exports = authentication;
