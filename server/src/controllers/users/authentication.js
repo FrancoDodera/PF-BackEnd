7a3c87fe19492882898c9a132c0ef3b531b85fab
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-25 13:32:25 -0400

    modificaciones auth

--- server/src/controllers/users/authentication.js
+++ server/src/controllers/users/authentication.js
@@ -5,28 +5,38 @@ const authentication=async (body)=>{
     const {name,lastName,email,user,token,dni,type,image,password}=body
     
     try {
-        const userFind=await User.findOne({user:user,tokenAuth:token});
-        if(userFind){
-            if(userFind.status){
-                return {acces:true,data:userFind}
-            }else{
-                return {acces:false,data:userFind}
-            }
-        }else{
-            const newUser = new User({
-                email:email,
-                name:name,
-                lastName:lastName,
-                dni:dni,
-                user:user,
-                password:password,
-                type:type,
-                image:image,
+        const emailFind=await User.findOne({email:email})
+        if(emailFind.tokenAuth == null){
+            const updateUser= await User.updateOne({_id:emailFind._id},{
                 tokenAuth:token
             })
-            await newUser.save()
-            return {acces:true,data:newUser}
+            const user=await User.findById(updateUser._id)
+            return {acces:true,data:user}
+        }else{
+            const userFind=await User.findOne({email:user,tokenAuth:token});
+            if(userFind){
+                if(userFind.status){
+                    return {acces:true,data:userFind}
+                }else{
+                    return {acces:false,data:userFind}
+                }
+            }else{
+                const newUser = new User({
+                    email:email,
+                    name:name,
+                    lastName:lastName,
+                    dni:dni,
+                    user:user,
+                    password:password,
+                    type:type,
+                    image:image,
+                    tokenAuth:token
+                })
+                await newUser.save()
+                return {acces:true,data:newUser}
+            }
         }
+        
     } catch (error) {
         throw error
     }
