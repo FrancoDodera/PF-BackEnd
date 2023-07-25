2734e2b99216ec6c4ba40168d301b85e8c66cdc8
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-25 12:54:21 -0400

    modificaciones auth

--- server/src/controllers/users/AddUser.js
+++ server/src/controllers/users/AddUser.js
@@ -1,36 +1,33 @@
 const User = require('../../models/UserModel')
 
 const addNewUser =async (req,res) =>{
+    try {
     const {email,name,lastName,dni,user,password,type,image}=req.body
     
-    const equalUser = await User.findOne({user:user})
-
+        const equalUser = await User.findOne({user:user})
         if(equalUser) {
-            if (equalUser.status===false) return res.status(200).send('inactive user')
-            if(equalUser.password==password) return res.status(202).send({acces:true,data:equalUser})
-            else{
-                return res.send('User found but wrong password')
-            }
+            return res.send('User already exists')
+        }else{
+            if (!email)  return res.status(400).send(`enter a email`)
+            if (!name)  return res.status(400).send(`enter a name`)
+            if (!lastName) return res.status(400).send(`enter a lastName`)
+            if (!user) return res.status(400).send(`enter a user`)
+            if (!password) return res.status(400).send(`enter a password`)
+            if (!type) return res.status(400).send(`enter a type`)
+            const newUser = new User({
+                email:email,
+                name:name,
+                lastName:lastName,
+                dni:dni,
+                user:user,
+                password:password,
+                type:type,
+                image:image,
+                tokenAuth:null
+            })
+            await newUser.save()
+            return res.status(200).send({acces:true})
         }
-        if (!email)  return res.status(400).send(`enter a email`)
-        if (!name)  return res.status(400).send(`enter a name`)
-        if (!lastName) return res.status(400).send(`enter a lastName`)
-        if (!user) return res.status(400).send(`enter a user`)
-        if (!password) return res.status(400).send(`enter a password`)
-        if (!type) return res.status(400).send(`enter a type`)
-    try {
-        const newUser = new User({
-            email:email,
-            name:name,
-            lastName:lastName,
-            dni:dni,
-            user:user,
-            password:password,
-            type:type,
-            image:image
-        })
-        await newUser.save()
-        return res.status(200).send({acces:true,data:newUser})
     } catch (error) {
         return res.status(400).send('the user is not logged in')
     }
