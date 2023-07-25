40f993a31bb15df5dfa6bc44ab9b0da409f15054
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-24 23:21:05 -0400

    cambios news

--- server/src/controllers/mercadopago/mercadopagoController.js
+++ server/src/controllers/mercadopago/mercadopagoController.js
@@ -58,9 +58,9 @@ const postPreference = async (req, res) => {
     const preference = {
       items: articuls,
       back_urls: {
-        success: "http://localhost:5173/success",
-        failure: "http://localhost:5173/failure",
-        pending: "http://localhost:5173/pending",
+        success: "https://pf-front-end-nine.vercel.app/success",
+        failure: "https://pf-front-end-nine.vercel.app/failure",
+        pending: "https://pf-front-end-nine.vercel.app/pending",
       },
       auto_return: "approved",
     };
