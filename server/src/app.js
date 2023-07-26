782c10218830fd14d93e7176f88126d152832ab1
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-26 01:38:57 -0400

    modificaciones reviews

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const mercadopagoRoutes = require('./routes/mercadopagoRoutes/mercadopagoRoutes'
 app.use(express.json());
 // // ... Otros middlewares
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); 
+  res.header('Access-Control-Allow-Origin', 'https://pfcargo.netlify.app'); 
   // res.header('Access-Control-Allow-Origin', 'https://superb-cuchufli-d6ef80.netlify.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
