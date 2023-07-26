814991824cea241297c8fc07c3bab9b068baf4b7
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-26 19:08:04 -0400

    modificaciones reviews

--- server/src/controllers/mercadopago/mercadopagoController.js
+++ server/src/controllers/mercadopago/mercadopagoController.js
@@ -55,24 +55,24 @@ const postPreference = async (req, res) => {
                 }
         }
 
-    // const preference = {
-    //   items: articuls,
-    //   back_urls: {
-    //     success: "https://pfcargo.netlify.app/success",
-    //     failure: "https://pfcargo.netlify.app/failure",
-    //     pending: "https://pfcargo.netlify.app/pending",
-    //   },
-    //   auto_return: "approved",
-    // };
     const preference = {
       items: articuls,
       back_urls: {
-        success: "http://localhost:5173/success",
-        failure: "http://localhost:5173/failure",
-        pending: "http://localhost:5173/pending",
+        success: "https://pfcargo.netlify.app/success",
+        failure: "https://pfcargo.netlify.app/failure",
+        pending: "https://pfcargo.netlify.app/pending",
       },
       auto_return: "approved",
     };
+    // const preference = {
+    //   items: articuls,
+    //   back_urls: {
+    //     success: "http://localhost:5173/success",
+    //     failure: "http://localhost:5173/failure",
+    //     pending: "http://localhost:5173/pending",
+    //   },
+    //   auto_return: "approved",
+    // };
 
     mercadopago.preferences
       .create(preference)
